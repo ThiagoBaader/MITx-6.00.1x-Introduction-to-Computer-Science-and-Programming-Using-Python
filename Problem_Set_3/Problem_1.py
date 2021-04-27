a1f9c8a5c9cad3c0fa17c1d4e27b4755b4441b9c
Thiago Baader <thiago_andre_baader@hotmail.com>
2021-04-27 15:41:37 -0300

    Update Problem_1.py

--- Problem_Set_3/Problem_1.py
+++ Problem_Set_3/Problem_1.py
@@ -13,17 +13,14 @@
 
 # For this function, you may assume that all the letters in secretWord and lettersGuessed are lowercase.
 
-def getGuessedWord(secretWord, lettersGuessed):
+def isWordGuessed(secretWord, lettersGuessed):
     '''
     secretWord: string, the word the user is guessing
     lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters and underscores that represents
-      what letters in secretWord have been guessed so far.
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
     '''
-    guessedWord = ''
     for letter in secretWord:
-        if letter in lettersGuessed:
-            guessedWord += letter
-        else:
-            guessedWord += '_ '
-    return guessedWord
+        if letter not in lettersGuessed:
+            return False
+    return True
